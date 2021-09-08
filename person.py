16649345627e97bd85150710700caf0364b03010
Hinn <64025546+TheHinn@users.noreply.github.com>
2021-09-08 09:57:56 +0200

    Added person.py

---
+++ person.py
@@ -0,0 +1,22 @@
+print('Naaminvoer')
+name = input('Geef jouw naam')
+print('Hallo ' + name)
+
+print('Adres')
+adress = input('Uw Adres?')
+print(adress)
+
+print('Postcode')
+postcodee = input('Postcode?')
+print(postcodee)
+
+print('Woonplaats')
+woonplaatss = input('Waar woont u?')
+print(woonplaatss) 
+
+print("----------------------------------------------------")
+print("|   Naam          : " + name)
+print("|   Adres         :  "+ adress)
+print("|   Postcode      :  " + postcodee)
+print("|   Woonplaats    :  " + woonplaatss)
+print("----------------------------------------------------")
